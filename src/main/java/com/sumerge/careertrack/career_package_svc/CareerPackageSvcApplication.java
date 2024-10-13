f3098e016c4f9111b198968db91d9ee83bf2185b
Mohamed Ashraf <mohamedashraff.hussein@gmail.com>
2024-10-13 11:20:22 +0300

    project setup

---
+++ src/main/java/com/sumerge/careertrack/career_package_svc/CareerPackageSvcApplication.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.career_package_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerPackageSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerPackageSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumerge/careertrack/career_package_svc/CareerPackageSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumerge.careertrack.career_package_svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerPackageSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
